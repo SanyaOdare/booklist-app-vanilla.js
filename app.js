7c5090f3ad083c4c8bb88924336d93abdc4f030f
sanyaodare@gmail.com <sanyaodare@gmail.com>
2020-09-17 17:04:01 +0100

    add store class

--- app.js
+++ app.js
@@ -49,6 +49,17 @@ class UI {
     }
   }
 
+  static showAlert(message, className) {
+    const div = document.createElement('div');
+    div.className = `alert alert-${className}`;
+    div.appendChild(document.createTextNode(message));
+    const container = document.querySelector('.container');
+    const form = document.querySelector('#book-form');
+    container.insertBefore(div, form);
+    // Vanish in 3 seconds
+    setTimeout(() => document.querySelector('.alert').remove(),3000);
+  }
+
   static clearFields() {
     document.querySelector('#title').value = '';
     document.querySelector('#author').value = '';
@@ -57,36 +68,64 @@ class UI {
 }
 
 // Store Class: Handles Storage
+class Store {
+  static getBooks() {
+    let books;
+    if(localStorage.getItem('books') === null) {
+      books = [];      
+    } else {
+      books = JSON.parse(localStorage.getItem('books'));
+    }
+
+    return books;
+  }
+
+  static addBook(book) {
+    const books = Store.getBooks();
+    books.push(book);
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+
+  static removeBook(isbn) {
+
+  }
+}
 
 // Event: Display Books
 document.addEventListener('DOMContentLoaded', UI.displayBooks);
 
 // Event: Add a Book
-document.querySelector('#book-form').addEventListener('submit', (e) => {
+document.querySelector("#book-form").addEventListener("submit", (e) => {
   // Prevent actual submit
   e.preventDefault();
 
   // Get form values
-  const title = document.querySelector('#title').value;
-  const author = document.querySelector('#author').value;
-  const isbn = document.querySelector('#isbn').value;
+  const title = document.querySelector("#title").value;
+  const author = document.querySelector("#author").value;
+  const isbn = document.querySelector("#isbn").value;
 
   // Validate
-  if(title === '' || author === '' || isbn === '') {
-    alert('Please fill in all fields');
+  if (title === "" || author === "" || isbn === "") {
+    UI.showAlert('Please fill in all fields', 'danger');
   } else {
     // Instantiate book
-  const book = new Book(title, author, isbn);
+    const book = new Book(title, author, isbn);
 
-  // Add Book to Ui
-  UI.addBookToList(book);
+    // Add Book to Ui
+    UI.addBookToList(book);
 
-  // Clear fields
-  UI.clearFields();
+    // Show success message
+    UI.showAlert('Book Added', 'success');
+
+    // Clear fields
+    UI.clearFields();
   }
 });
 
 // Event: Remove a Book
 document.querySelector('#book-list').addEventListener('click', (e) => {
-  UI.deleteBook(e.target)
+  UI.deleteBook(e.target);
+
+  // Show success message
+  UI.showAlert('Book Removed', 'success');
 });
