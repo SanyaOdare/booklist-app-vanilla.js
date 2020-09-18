d39f54bba5a02980d9141b59bd33fd256902b6f9
sanyaodare@gmail.com <sanyaodare@gmail.com>
2020-09-18 18:33:37 +0100

    persist data in locallocalStorage

--- app.js
+++ app.js
@@ -10,20 +10,7 @@ class Book {
 // UI Class: Handle UI Tasks
 class UI {
   static displayBooks() {
-    const StoredBooks = [
-      {
-        title: 'Book One',
-        author: 'John Doe',
-        isbn: '3434434'
-      },
-      {
-        title: 'Book Two',
-        author: 'Jane Doe',
-        isbn: '45545'
-      }
-    ];
-
-    const books = StoredBooks;
+    const books = Store.getBooks();    
 
     books.forEach((book) => UI.addBookToList(book));
   }
@@ -56,6 +43,7 @@ class UI {
     const container = document.querySelector('.container');
     const form = document.querySelector('#book-form');
     container.insertBefore(div, form);
+
     // Vanish in 3 seconds
     setTimeout(() => document.querySelector('.alert').remove(),3000);
   }
@@ -87,7 +75,15 @@ class Store {
   }
 
   static removeBook(isbn) {
+    const books = Store.getBooks();
 
+    books.forEach((book, index) => {
+      if(book.isbn === isbn) {
+        books.splice(index, 1);
+      }
+    });
+
+    localStorage.setItem('books', JSON.stringify(books));
   }
 }
 
@@ -114,6 +110,9 @@ document.querySelector("#book-form").addEventListener("submit", (e) => {
     // Add Book to Ui
     UI.addBookToList(book);
 
+    // Add book to store
+    Store.addBook(book);
+
     // Show success message
     UI.showAlert('Book Added', 'success');
 
@@ -124,7 +123,11 @@ document.querySelector("#book-form").addEventListener("submit", (e) => {
 
 // Event: Remove a Book
 document.querySelector('#book-list').addEventListener('click', (e) => {
+  // Remove book from UI
   UI.deleteBook(e.target);
+
+  // Remove book from store
+  Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
 
   // Show success message
   UI.showAlert('Book Removed', 'success');
